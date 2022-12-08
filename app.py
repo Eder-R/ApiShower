0fd89c7c59705481005ad0b1ec6269050e29f24b
Eder-R <eder31250@gmail.com>
2022-12-08 19:53:45 -0300

    First Function created

---
+++ app.py
@@ -0,0 +1,15 @@
+class Tempo():
+  def runner():
+    name = ""
+    run = Timer()
+
+    if name == "Edu":
+      run(5000)
+      
+    elif name == "Eder":
+      run(10000)
+      
+    else print('Tag não cadastrada')
+    
+if __name__ == '__main__':
+  app.Tempo()
